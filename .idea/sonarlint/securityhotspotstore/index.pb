dfff7c5cb3dfe104576f3968264f4aa54fc78278
Bruno <dantebruno10@gmail.com>
2025-02-05 19:47:25 -0300

    Tarefa conlcuida

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+\
+,src/br/com/empresa/Calcula/CalculaMedia.java,0\f\0f8c9d12fddfe4bd840666caf3850414507af4fa
---
+++ src/br/com/empresa/Calcula/CalculaMedia.java
@@ -0,0 +1,26 @@
+package br.com.empresa.Calcula;
+
+public class CalculaMedia {
+
+    public static void main(String[] args) {
+
+        double nota1 = 10;
+        double nota2  = 9;
+        double nota3 = 8;
+        double nota4 = 7;
+
+        int media = 4;
+
+        double media1 = nota1 / media;
+        double media2 = nota2 / media;
+        double media3 = nota3 / media;
+        double media4 = nota4 / media;
+
+        System.out.println("Media 1: " + media1
+                            + "\nMédia 2: " + media2
+                            + "\nMédia 3: " + media3
+                            + "\nMédia 4: " + media4);
+
+    }
+
+}
